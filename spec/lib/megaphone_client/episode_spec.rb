d09e790f8d396d876ae870e62df5ee0662c3dafb
arellaTV <jeremiah.arella@gmail.com>
2018-05-09 15:20:52 -0700

    Adds delete functionality and bumps minor versioning

--- spec/lib/megaphone_client/episode_spec.rb
+++ spec/lib/megaphone_client/episode_spec.rb
@@ -53,6 +53,34 @@ describe MegaphoneClient::Episode do
     end
   end
 
+  describe "delete" do
+    request_uri = "https://cms.megaphone.fm/api/networks/STUB_NETWORK_ID/podcasts/STUB_PODCAST_ID/episodes/STUB_EPISODE_ID"
+
+    before :each do
+      @megaphone = MegaphoneClient.new({ network_id: "STUB_NETWORK_ID" })
+      @episodes = @megaphone.episodes
+    end
+
+    it "should return an ArgumentError if no podcast_id or episode_id is given" do
+      expect { @megaphone.episodes.delete }.to raise_error(ArgumentError)
+    end
+
+    it "should only perform DELETE requests" do
+      VCR.use_cassette("delete_result_01") do
+        @megaphone.episodes.delete({
+          podcast_id: "STUB_PODCAST_ID",
+          episode_id: "STUB_EPISODE_ID"
+        })
+
+        expect(WebMock).to have_requested(:delete, request_uri)
+        expect(WebMock).not_to have_requested(:get, request_uri)
+        expect(WebMock).not_to have_requested(:patch, request_uri)
+        expect(WebMock).not_to have_requested(:post, request_uri)
+        expect(WebMock).not_to have_requested(:put, request_uri)
+      end
+    end
+  end
+
   describe "search" do
     before :each do
       @megaphone = MegaphoneClient.new
